a6afad9df34fbe1d0a86569422913194815353df
Happy-Lotus <happylotus145@gmail.com>
2025-07-17 02:01:05 +0900

    [Feature] 프론트엔드 결제수단 sdk 호출, DB 저장, 사용자의 결제수단 전체조회 연결

--- frontend/src/components/Payment/PaymentMethodList.jsx
+++ frontend/src/components/Payment/PaymentMethodList.jsx
@@ -1,27 +1,24 @@
-import React,{useEffect, useState} from "react";
-import styles from "./paymentMethod.module.css";
-import PaymentMethodCard from "./PaymentMethodCard";
-// import getPaymentMethods from "../../api/portone";
+import React, { useEffect, useState } from 'react';
+import styles from './paymentMethod.module.css';
 
 // 카드 리스트(결제수단관리))
-function PaymentMethodList() {
-    const [cards, setCards] = useState([]); // 사용자 카드 목록
-    const userId = 1; //임시 userId
-
-    useEffect(() => {
-        // getPaymentMethods()
-        // .then(setCards)
-        // .catch(err => console.error(err));
-        
-    }, [])
-    
-    return (
-        <div className={styles.cardList_container}>
-            {cards.map((card, i) => (
-                 <PaymentMethodCard key={i} card={card} />
-             ))}
+function PaymentMethodList({ cards }) {
+  return (
+    <div className={styles.card_list_container}>
+      {cards.length === 0 && <p>등록된 카드가 없습니다.</p>}
+      {cards.map((card, idx) => (
+        <div key={idx} className="card-box">
+          <p>
+            <strong>{card.cardName}</strong>
+          </p>
+          <p>
+            {card.cardBrand} / {card.cardType}
+          </p>
+          <p>{card.cardNumber}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default PaymentMethodList;
--- frontend/src/pages/Payment/PaymentMethodManagementPage.jsx
+++ frontend/src/pages/Payment/PaymentMethodManagementPage.jsx
@@ -1,38 +1,56 @@
-import React, { useEffect } from "react";
-import styles from "./paymentMethodManagementPage.module.css";
-import PaymentMethodList from "../../components/Payment/PaymentMethodList";
-import { requestIssueBillingKey } from "../../api/portone";
+import React, { useEffect, useState } from 'react';
+import styles from './paymentMethodManagementPage.module.css';
+import PaymentMethodList from '../../components/Payment/PaymentMethodList';
+import { requestIssueBillingKey, savePaymentMethod, readAllPaymentMethod } from '../../api/portone';
 
 // paymentMethodManagement page
 function PaymentMethodManagementPage() {
+  const [cards, setCards] = useState([]);
+  const storeId = import.meta.env.VITE_STORE_ID;
+  const channelKey = import.meta.env.VITE_CHANNEL_KEY;
 
-    const handleCardRegister = async () => {
-        const issueResponse = await requestIssueBillingKey();
+  // 컴포넌트가 마운트될 때 카드 정보 불러오기
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const res = await readAllPaymentMethod(); // API 호출
+        setCards(res); // 받아온 카드 정보를 상태에 저장
+      } catch (err) {
+        console.error('카드 정보를 불러오는 데 실패했습니다.', err);
+      }
+    };
+    fetchCards();
+  }, []); // 빈 배열 → 최초 한 번만 실행됨
 
-        if (issueResponse.code !== undefined) {
-            alert(issueResponse.message)
-            return;
-        }
-        alert(issueResponse.billingKey)
-        }
-    
-    return (
-        <div className={styles.payment_container}>
-            {/* 결제수단관리 헤더 */}
-            <div className={styles.header}>
-                <p className={styles.title}>결제수단 관리</p>
-                <p className={styles.subTitle}>등록된 결제수단을 관리하고 새로운 결제수단을 추가할 수 있습니다</p>
-            </div>
+  //빌링키 발급 후 결제수단 조회해 보여주는 메소드
+  const handleCardRegister = async () => {
+    // 빌링키 발급
+    const issueResponse = await requestIssueBillingKey(storeId, channelKey, 'test', 'test@gmail.com');
+    // 발급된 빌링키 이용한 결제수단 조회
+    const saveResponse = await savePaymentMethod(issueResponse.billingKey);
 
-            {/* 등록된 카드 리스트 */}
-            <PaymentMethodList />
-            <div className={styles.register}>
-                <button className={styles.registerButton} onClick={handleCardRegister}>
-                    <p>카드 등록</p>
-                </button>
-            </div>
-        </div>
-    );    
+    const { username, ...cardWithoutUsername } = saveResponse;
+
+    setCards(prev => [...prev, cardWithoutUsername]);
+  };
+
+  return (
+    <div className={styles.payment_container}>
+      {/* 결제수단관리 헤더 */}
+      <div className={styles.header}>
+        <p className={styles.title}>결제수단 관리</p>
+        <p className={styles.sub_title}>등록된 결제수단을 관리하고 새로운 결제수단을 추가할 수 있습니다</p>
+      </div>
+
+      {/* 등록된 카드 리스트 */}
+      <PaymentMethodList cards={cards} />
+      <div className={styles.register}>
+        <button className={styles.register_button} onClick={handleCardRegister}>
+          <p>카드 등록</p>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default PaymentMethodManagementPage;
