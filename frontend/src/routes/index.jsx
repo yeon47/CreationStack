6214dc6039fc40cdd475a2093967b479a08e4522
yeon47 <cerry0724@naver.com>
2025-07-17 10:32:13 +0900

    댓글좋아요+콘텐츠좋아요ui초안

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import MainLayout from '../layouts/mainLayout';
 import ReplyTestPage from '../pages/ContentDetail/ReplyTestPage';
+import FavoriteContent from '../pages/MyPage/FavoriteContent';
 
 export default function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ export default function AppRoutes() {
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/reply-test" element={<ReplyTestPage />} />
+        <Route path="/favorites" element={<FavoriteContent />} />
       </Route>
 
       {/* 로그인/회원가입 같이 레이아웃 제외할 페이지는 별도 Route로 */}
