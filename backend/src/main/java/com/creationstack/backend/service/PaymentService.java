06b1319b13091c3583abf31b347d23a2aa82065a
Happy-Lotus <happylotus145@gmail.com>
2025-07-18 10:08:21 +0900

    [Style] 테스트용 구독내역저장 로직 주석처리

--- backend/src/main/java/com/creationstack/backend/service/PaymentService.java
+++ backend/src/main/java/com/creationstack/backend/service/PaymentService.java
@@ -1,10 +1,9 @@
 package com.creationstack.backend.service;
 
 import com.creationstack.backend.config.PortOneClient;
-import com.creationstack.backend.domain.payment.Payment;
+
 import com.creationstack.backend.domain.payment.PaymentMethod;
-import com.creationstack.backend.domain.payment.PaymentStatus;
-import com.creationstack.backend.domain.payment.Subscription;
+
 import com.creationstack.backend.dto.Payment.BillingKeyPaymentRequestDto;
 import com.creationstack.backend.dto.Payment.BillingKeyPaymentResponseDto;
 import com.creationstack.backend.dto.Payment.CustomerDto;
@@ -12,11 +11,9 @@ import com.creationstack.backend.dto.Payment.CustomerDto.Name;
 import com.creationstack.backend.dto.Payment.PortOneBillingResponseDto;
 import com.creationstack.backend.dto.Payment.PortOnePaymentRequestDto;
 import com.creationstack.backend.dto.Payment.PortOnePaymentRequestDto.Amount;
-import com.creationstack.backend.repository.PaymentRepository;
-import com.creationstack.backend.repository.SubscriptionRepository;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -26,10 +23,10 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class PaymentService {
 
-  private final PaymentRepository paymentRepository;
+//  private final PaymentRepository paymentRepository;
   private final PaymentMethodService paymentMethodService;
   private final PortOneClient portOneClient;
-  private final SubscriptionRepository subscriptionRepository;
+//  private final SubscriptionRepository subscriptionRepository;
 
   //결제
   public BillingKeyPaymentResponseDto processingBillingKeyPay(BillingKeyPaymentRequestDto req) {
@@ -58,37 +55,37 @@ public class PaymentService {
     log.info("[processingBillingKeyPay] portOneClient.processingBillingKeyPay: {}", response);
     // 구독 내역 active 변경 로직
 
-    Subscription subscription = subscriptionRepository.findById(1L).orElse(null);
-    log.info("[processingBillingKeyPay] subscription: {}", subscription);
-    String rawDateTime = response.getResponse().get("payment").get("paidAt").asText();
+//    Subscription subscription = subscriptionRepository.findById(1L).orElse(null);
+//    log.info("[processingBillingKeyPay] subscription: {}", subscription);
+//    String rawDateTime = response.getResponse().get("payment").get("paidAt").asText();
+//
+//    Instant instant = Instant.parse(rawDateTime);
+//    LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.of("Asia/Seoul"));
+//    System.out.println("LocalTime: " + localDateTime);
+//    // 결제 내역 저장
+//    Payment payment = Payment.builder()
+//        .paymentMethod(paymentMethod)
+//        .subscription(subscription)
+//        .amount(req.getAmount())
+//        .paymentStatus(PaymentStatus.PENDING)
+//        .transactionId(response.getResponse().get("payment").get("pgTxId").asText())
+//        .failureReason(null)
+//        .tryAt(tryAt)
+//        .successAt(localDateTime)
+//        .build();
+//
+//    // 결제내역 저장
+//    Payment savePayment = paymentRepository.save(payment);
+//    log.info("[processingBillingKeyPay] savePayment: {}", savePayment);
+//
+//    //정기결제 예약
+//
+//
+//    BillingKeyPaymentResponseDto res = new BillingKeyPaymentResponseDto(
+//        savePayment.getPaymentId(),PaymentStatus.SUCCESS,payment.getTryAt(),payment.getSuccessAt(),req.getOrderName(),req.getAmount()
+//    );
 
-    Instant instant = Instant.parse(rawDateTime);
-    LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.of("Asia/Seoul"));
-    System.out.println("LocalTime: " + localDateTime);
-    // 결제 내역 저장
-    Payment payment = Payment.builder()
-        .paymentMethod(paymentMethod)
-        .subscription(subscription)
-        .amount(req.getAmount())
-        .paymentStatus(PaymentStatus.PENDING)
-        .transactionId(response.getResponse().get("payment").get("pgTxId").asText())
-        .failureReason(null)
-        .tryAt(tryAt)
-        .successAt(localDateTime)
-        .build();
-
-    // 결제내역 저장
-    Payment savePayment = paymentRepository.save(payment);
-    log.info("[processingBillingKeyPay] savePayment: {}", savePayment);
-
-    //정기결제 예약
-
-
-    BillingKeyPaymentResponseDto res = new BillingKeyPaymentResponseDto(
-        savePayment.getPaymentId(),PaymentStatus.SUCCESS,payment.getTryAt(),payment.getSuccessAt(),req.getOrderName(),req.getAmount()
-    );
-
-    return res;
+    return null;
   }
 
   //결제취소(구독취소)
