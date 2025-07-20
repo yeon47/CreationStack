69a0e571e7405b7345cf6b66d1c540ce0be6814b
Yennie <lye0713@gmail.com>
2025-07-20 22:24:05 +0900

    [Feature] 사용자 공개 프로필 조회
    [메서드 추가]
    - UserService
    - UserRepository
    - UserProfileController
    
    [파일 추가]
    - PublicProfileResponse

---
+++ backend/src/main/java/com/creationstack/backend/dto/member/PublicProfileResponse.java
@@ -0,0 +1,17 @@
+package com.creationstack.backend.dto.member;
+
+import com.creationstack.backend.domain.user.User;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PublicProfileResponse {
+    private String nickname;
+    private User.UserRole role;
+    private String jobName;
+    private String bio;
+        private String profileImageUrl;
+        private boolean isActive;
+}
